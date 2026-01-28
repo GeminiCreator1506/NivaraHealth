e7989a4a6f9e12dba90081ab583be04b41ce5903
Sashreek Satapathy <sashreeksatapathydpsnt15@gmail.com>
2026-01-28 22:41:50 +0530

    Add custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/NivaraHealth/",   // 👈 MUST MATCH REPO NAME
+  base: "/",
+  build: {
+    outDir: 'docs'
+  }
 });
-
